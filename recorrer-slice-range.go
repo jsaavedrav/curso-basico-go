54f57e4ed65c3ab27ec7c10370767f342e92cdaa
José Saavedra <jsaavedv@cl.ibm.com>
2021-08-06 02:04:05 -0400

    Se actualiza con recorrer slice

--- recorrer-slice-range.go
+++ recorrer-slice-range.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func main()  {
+func isPalindromo(texto string) {
+	var textoReversa string
+	for i := len(texto) - 1; i >= 0; i-- {
+		textoReversa += string(texto[i])
+	}
+	if strings.ToLower(textoReversa) == strings.ToLower(texto) {
+		fmt.Println("Es Palindromo")
+	} else {
+		fmt.Println("No es Palindromo")
+	}
+}
+
+func main() {
 	slice := []string{"hola", "que", "hace"}
 	for i, valor := range slice {
 		fmt.Println(i, valor)
@@ -12,4 +27,14 @@ func main()  {
 	for _, valor := range slice {
 		fmt.Println(valor)
 	}
+
+	// si no queremos mostrar el valor podemos hacer lo siguiente:
+	for i := range slice {
+		fmt.Println(i)
+	}
+
+	// Palindromo
+	// ama
+	// amor a roma
+	isPalindromo("Ana")
 }
